15a6da5c54cda07f7ba822ada213dd7858fdec31
jgtech1205 <jgtech1205@gmail.com>
2025-03-12 12:47:00 -0400

    Initial commit with project files

---
+++ tulos_yt/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="p-10">
+    <h2>Tulos ecommerce website</h2>
+    <button>Demo button</button>
+   </div>
+  );
+}
