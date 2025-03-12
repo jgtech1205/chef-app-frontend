541dec283aaa5082f038f16d0385ac65cdf2c6ee
jgtech1205 <jgtech1205@gmail.com>
2025-03-12 15:08:57 -0400

    Upated header

--- tulos_yt/app/page.tsx
+++ tulos_yt/app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="p-10">
+   <div>
     <h2>Tulos ecommerce website</h2>
-    <button>Demo button</button>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded">
+  Demo button
+</button>
+
    </div>
   );
 }
---
+++ tulos_yt/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderMenu from "@/components/HeaderMenu";
+import Logo from "@/components/Logo";
+
+const Header = () => {
+  return <header className="bg-white border-b border-b-gray-400 py-5">
+  
+   <HeaderMenu />
+   <Logo />
+   <div>right</div>
+  </header>;
+};
+
+export default Header;
